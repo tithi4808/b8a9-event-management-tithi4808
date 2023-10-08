77403b728fadf36b2e44cbc214071634361a991d
tithi2149 <tanyasultana60@gmail.com>
2023-10-09 00:41:08 +0600

    updated the animations in intro part

--- src/Components/Pages/Home/Intro/Intro.jsx
+++ src/Components/Pages/Home/Intro/Intro.jsx
@@ -12,12 +12,12 @@ const Intro = () => {
         });
       }, [])
     return (
-        <div className='mt-10  flex flex-col-reverse md:grid grid-cols-2 rounded-lg bg-slate-200 mx-6 mb-10'>
-            <div   className='grid grid-cols-2 md:grid-cols-1 gap-2 lg:grid-cols-2'>
-                <div><img className='h-56 w-96 rounded-lg'  src="https://i.ibb.co/LrHZ8Xk/20211107-Mengjie-Liu-T257.jpg" alt="" /></div>
-                <img className='h-56 w-96 rounded-lg' src="https://i.ibb.co/SPV9hWj/greece-wedding-santorini-photosession-10-1920x1080-0c6.jpg" alt="" />
-                <img className='h-56 w-96 rounded-lg' src="https://i.ibb.co/Zf154rH/1707.jpg" alt="" />
-                <img className='h-56 w-96 rounded-lg' src="https://i.ibb.co/SPn1fDJ/download-1.jpg" alt="" />
+        <div className='mt-10  flex flex-col-reverse lg:grid grid-cols-2 rounded-lg bg-slate-200 mx-6 mb-10'>
+            <div   className='grid grid-cols-2  gap-2 md::grid-cols-2'>
+                <div  data-aos="fade-right"><img className='h-56 w-96 rounded-lg'  src="https://i.ibb.co/LrHZ8Xk/20211107-Mengjie-Liu-T257.jpg" alt="" /></div>
+                <img data-aos="fade-left" className='h-56 w-96 rounded-lg' src="https://i.ibb.co/SPV9hWj/greece-wedding-santorini-photosession-10-1920x1080-0c6.jpg" alt="" />
+                <img data-aos="fade-right" className='h-56 w-96 rounded-lg' src="https://i.ibb.co/Zf154rH/1707.jpg" alt="" />
+                <img data-aos="fade-left" className='h-56 w-96 rounded-lg' src="https://i.ibb.co/SPn1fDJ/download-1.jpg" alt="" />
             </div>
            <div  className='bg-sky-100 p-16 rounded-lg '>
            <p className='font-bold text-3xl text-end mb-4 mt-8' data-aos="fade-up">WELCOME TO WINK EVENT MANAGEMENT</p>
